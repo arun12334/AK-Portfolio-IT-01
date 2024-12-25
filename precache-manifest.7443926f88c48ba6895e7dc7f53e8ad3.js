1097b119881919ab06fa9e8d7613daf730743de1
arun12334 <arunaakash114@gmail.com>
2024-12-25 20:38:11 +0530

    Updates

---
+++ precache-manifest.7443926f88c48ba6895e7dc7f53e8ad3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "864430d80972f87bfe0c",
+    "url": "/AK-Portfolio-IT-01/static/css/main.0a31525d.chunk.css"
+  },
+  {
+    "revision": "864430d80972f87bfe0c",
+    "url": "/AK-Portfolio-IT-01/static/js/main.864430d8.chunk.js"
+  },
+  {
+    "revision": "7e660683b4730355bd3f",
+    "url": "/AK-Portfolio-IT-01/static/js/1.7e660683.chunk.js"
+  },
+  {
+    "revision": "ab1b177d6521f4516c83",
+    "url": "/AK-Portfolio-IT-01/static/js/runtime~main.ab1b177d.js"
+  },
+  {
+    "revision": "cc12580b55e0de8787f3687ed49d4c29",
+    "url": "/AK-Portfolio-IT-01/static/media/code1.cc12580b.gif"
+  },
+  {
+    "revision": "2adff04e633db308e4bf650b80903b7c",
+    "url": "/AK-Portfolio-IT-01/static/media/arun.2adff04e.pdf"
+  },
+  {
+    "revision": "3f02797c8b7a651c0a23fb460d7400c2",
+    "url": "/AK-Portfolio-IT-01/static/media/milkball.3f02797c.webm"
+  },
+  {
+    "revision": "a3a871762f372e73511cfcd321ec7b7e",
+    "url": "/AK-Portfolio-IT-01/static/media/contact5.a3a87176.gif"
+  },
+  {
+    "revision": "46efc84b5a385e60dfaae6c0a8f3bf10",
+    "url": "/AK-Portfolio-IT-01/static/media/contact.46efc84b.gif"
+  },
+  {
+    "revision": "65fdc4522749daf4595f76c4faacf36b",
+    "url": "/AK-Portfolio-IT-01/static/media/contact4.65fdc452.gif"
+  },
+  {
+    "revision": "479780de3cc7f118b7e6508734e6a68f",
+    "url": "/AK-Portfolio-IT-01/static/media/contact2.479780de.gif"
+  },
+  {
+    "revision": "b08fc24329b32562dee9813c7249306c",
+    "url": "/AK-Portfolio-IT-01/static/media/contact3.b08fc243.gif"
+  },
+  {
+    "revision": "238f4fa1783b7180e37a3e9dbe10a8a2",
+    "url": "/AK-Portfolio-IT-01/index.html"
+  }
+];
